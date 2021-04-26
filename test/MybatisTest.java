1d7c79ed79a7e05a679bc8849b46b93cebabeda8
hzy <15631166025@163.com>
2021-04-26 21:48:13 +0800

    test

--- test/MybatisTest.java
+++ test/MybatisTest.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -7,7 +8,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import cn.hzy.demo.model.Account;
 import cn.hzy.demo.model.Customer;
 
 public class MybatisTest {
@@ -27,5 +27,24 @@ public class MybatisTest {
 		
 		Customer customer = sqlSession.selectOne("cn.hzy.demo.mapper.CustomerMapper.findCustomerById", 1);
 		System.out.println(customer);
+	}
+	
+	@Test
+	public void findByNameTest() {
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream("mybatis/mybatis-config.xml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession sqlSession = sessionFactory.openSession();
+		Customer cus = new Customer();
+		cus.setName("s");
+		cus.setJobs("teacher");
+		List<Customer> customers = sqlSession
+				.selectList("cn.hzy.demo.mapper.CustomerMapper.findCustomerByName", cus);
+		System.out.println(customers);
 	}
 }
